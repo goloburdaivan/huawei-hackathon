3618826b22174fab81771b0cdbc4c9fb6ecd9070
Ruslan Zhadko <125014962+rzhadkpi@users.noreply.github.com>
2024-10-31 22:55:44 +0200

    Update polling_service.go

--- internal/services/polling_service.go
+++ internal/services/polling_service.go
@@ -104,7 +104,5 @@ func (p *PollingService) GetPortStats() []structs.PortInfo {
 }
 
 func (p *PollingService) IsValidPortIndex(index int) bool {
-	p.mu.RLock()
-	defer p.mu.RUnlock()
 	return index >= 0 && index < len(p.portStats)
 }
